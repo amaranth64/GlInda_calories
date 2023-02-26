9c4b36df0f042c71e1158b8da55a0e91d30b7502
Vlad Borisov <amaranth64@yandex.ru>
2023-02-26 11:00:43 +0400

    сделан xml layout file

--- app/src/main/java/ru/worklight64/calories/StepByStepActivity.kt
+++ app/src/main/java/ru/worklight64/calories/StepByStepActivity.kt
@@ -2,10 +2,13 @@ package ru.worklight64.calories
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.worklight64.calories.databinding.ActivityStepByStepBinding
 
 class StepByStepActivity : AppCompatActivity() {
+    private lateinit var form: ActivityStepByStepBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_step_by_step)
+        form = ActivityStepByStepBinding.inflate(layoutInflater)
+        setContentView(form.root)
     }
 }
